547eff1bc757a473387d67b80527dfbc76f08823
FIlip Demski <glamhoth@protonmail.com>
2021-01-28 16:10:50 +0100

    Build: Created skeleton

---
+++ cmake/CompilerWarnings.cmake
@@ -0,0 +1,39 @@
+option(OPTIONS_WARNINGS_AS_ERRORS
+       "Treat compiler wargnings as errors"
+       TRUE)
+
+set(CLANG_WARNINGS -Wall
+                   -Wextra
+                   -Wpedantic
+                   -Wshadow
+                   -Wold-style-cast
+                   -Wcast-align
+                   -Wunused
+                   -Wsign-conversion
+                   -Wnull-dereference
+                   -Wdouble-promotion
+                   -Wformat=2
+                   -Wno-c99-extensions
+   )
+
+if(WARNINGS_AS_ERRORS)
+    set(CLANG_WARNINGS ${CLANG_WARNINGS} -Werror)
+endif()
+
+set(GCC_WARNINGS ${CLANG_WARNINGS}
+                 -Wmisleading-indentation
+                 -Wduplicated-cond
+                 -Wduplicated-branches
+                 -Wlogical-op
+                 -Wuseless-cast
+   )
+
+if(MSVC)
+    set(PROJECT_WARNINGS -w)
+elseif(CMAKE_C_COMPILER_ID STREQUAL "Clang")
+    set(PROJECT_WARNINGS ${CLANG_WARNINGS})
+else()
+    set(PROJECT_WARNINGS ${GCC_WARNINGS})
+endif()
+
+target_compile_options(common_project_options INTERFACE ${PROJECT_WARNINGS})
---
+++ cmake/CppCheck.cmake
@@ -0,0 +1,23 @@
+option(TOOLS_ENABLE_CPPCHECK
+       "Enable static analysis with cppcheck"
+       FALSE
+      )
+
+if(TOOLS_ENABLE_CPPCHECK)
+    find_program(CPPCHECK cppcheck)
+    if(CPPCHECK)
+        log_option_enabled("cppcheck")
+        set(CMAKE_C_CPPCHECK ${CPPCHECK}
+                             --enable=all
+                             --inconclusive
+                             --force
+                             --quiet
+                             --inline-suppr
+                             --suppressions-list=${CMAKE_SOURCE_DIR}/suppressions.txt
+           )
+    else()
+        log_program_missing("cppcheck")
+    endif()
+else()
+    log_option_disabled("cppcheck")
+endif()
